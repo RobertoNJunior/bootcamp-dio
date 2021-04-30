0023e226649dfb11e8aab4d9f64a0cf5e75f481e
bob-roberto <roberto.junior@exactaworks.com.br>
2021-04-30 17:24:44 -0300

    add crossOrigin

--- src/main/java/com/project/bootcamp/controller/ActiveController.java
+++ src/main/java/com/project/bootcamp/controller/ActiveController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import javax.validation.Valid;
 import java.util.List;
 
+@CrossOrigin
 @RestController
 @RequestMapping(value = "/active")
 public class ActiveController {
